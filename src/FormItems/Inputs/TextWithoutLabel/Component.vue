2fa8bb6577c38fbc5095063965185e913d67f35d
József Bagota <jozsef.bagota@gmail.com>
2023-04-30 19:50:48 +0200

    creating two form components without label.

---
+++ src/FormItems/Inputs/TextWithoutLabel/Component.vue
@@ -0,0 +1,9 @@
+<script>
+    import TextWithoutLabel from './../../../../../helper-vue-components/src/Form/BootstrapFormItems/FormItemsWithoutLabel/Inputs/Text/Component.vue'
+
+    export default {
+        mixins: [
+            TextWithoutLabel,
+        ]
+    }
+</script>
